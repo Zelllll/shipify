9b497a4d3f640f5372dd3d75421a9ba68bd0acfc
Zelllll <56516451+Zelllll@users.noreply.github.com>
2025-01-04 14:33:19 -0600

    Add colored output 2

--- src/OutputHelpers.java
+++ src/OutputHelpers.java
@@ -8,21 +8,6 @@ public class OutputHelpers {
     public static final String RED = "\033[0;31m";
     public static final String BOLD = "\033[1m";
 
-//    public static void main(String[] args) {
-//        System.out.println(YELLOW + "Input directory verified: " + CYAN + "C:\\Users\\Elijah\\CLionProjects\\ddpc2\\patch_assets" + RESET);
-//        System.out.println(YELLOW + "Output directory verified: " + CYAN + "C:\\Users\\Elijah\\CLionProjects\\ddpc2\\patch_assets\\out" + RESET);
-//        System.out.println(BLUE + "Found " + BOLD + "97 files" + RESET + BLUE + " in input directory." + RESET);
-//        printProgress("Building scenes", GREEN);
-//        printProgress("Building objects", GREEN);
-//        printProgress("Building miscellaneous files", GREEN);
-//        printProgress("Building text", GREEN);
-//        printProgress("Building audio", GREEN);
-//        printProgress("Building entrance table", GREEN);
-//        printProgress("Building entrance cutscene table", GREEN);
-//        System.out.println(RED + "Withdrawing $1,000..." + RESET);
-//        System.out.println(GREEN + BOLD + "Success!" + RESET + " Output generated in: " + CYAN + "C:\\Users\\Elijah\\CLionProjects\\ddpc2\\patch_assets\\out" + RESET);
-//    }
-
     public static void printProgress(String task) {
         System.out.println(GREEN + task + "..." + RESET);
     }
